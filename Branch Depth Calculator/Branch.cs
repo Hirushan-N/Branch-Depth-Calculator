6db7ae2b1f39d90ccead4db588b004554283dd51
Hirushan-N <78243763+Hirushan-N@users.noreply.github.com>
2023-03-20 16:59:00 +0530

    Create Branch.cs

---
+++ Branch Depth Calculator/Branch.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Branch_Depth_Calculator
+{
+    public class Branch
+    {
+        public List<Branch> branches = new List<Branch>();
+
+        public int CalculateDepth()
+        {
+            int depth = 1;
+            int maxDepth = 0;
+            foreach (Branch b in this.branches)
+            {
+                int d = b.CalculateDepth();
+                if (d > maxDepth)
+                {
+                    maxDepth = d;
+                }
+            }
+            depth += maxDepth;
+            return depth;
+        }
+    }
+}
